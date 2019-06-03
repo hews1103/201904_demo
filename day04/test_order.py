0c364cef7e2da78c26f5bdbedd7e2eab2ea00fd0
何文双 <1255790661@qq.com>
2019-06-03 19:39:36 +0800

    day04 over

--- day04/test_order.py
+++ day04/test_order.py
@@ -7,13 +7,13 @@ class Test_order(object):
     def test_order_add(self):
         # 假设发送了请求
         # 假设获得了响应
-        assert '成功' in '操作成功'
+        assert '不成功' in '操作成功'
 
     @allure.story('待发货')
     def test_order_demo(self):
         # 假设发送了请求
         # 假设获得了响应
         try:
-            assert '雨女' in '雨女无瓜'
+            assert '无瓜' in '雨女无瓜'
         except:
             print('响应错误')
